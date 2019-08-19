247de60f1bdfe9a856a9063155494a692207b2cd
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-08-19 21:58:36 +0100

    Abaut page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   root to: 'posts#index'
+  get 'about', to: 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
